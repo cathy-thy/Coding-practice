6fbd9403ed6b9ee471eb6dbafe2dd726fd44b16e
cathytsui <thy1030@gmail.com>
2022-04-21 00:39:18 +0800

    React demo

---
+++ React-JS-practice/basicreactdemo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <marquee><h5>Welcome To React practice</h5></marquee>
+      <hr color="red" size="3"/>
+    </div>
+  );
+}
+
+export default App;

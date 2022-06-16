d03281bc3be2d98645614be28eefc4fa4de9c8e6
cathytsui <48180241+cathy-thy@users.noreply.github.com>
2022-06-17 00:17:21 +0800

    complete inventory service

--- Angular-practice/demoapp/src/app/servicecommunication/productlistitem/productlistitem.component.ts
+++ Angular-practice/demoapp/src/app/servicecommunication/productlistitem/productlistitem.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { InventoryService } from 'src/shared/inventory.service';
 import { Product } from 'src/shared/product.module';
 
 @Component({
@@ -8,9 +9,17 @@ import { Product } from 'src/shared/product.module';
 })
 export class ProductlistitemComponent implements OnInit {
   @Input() product:Product
-  constructor() { }
+
+  //we have to pass current product into here
+  constructor(private inventoryservice:InventoryService) { }
 
   ngOnInit(): void {
+  }
+
+  
+  onOrderStock()
+  {
+    this.inventoryservice.addOrder(this.product);
   }
 
 }
--- Angular-practice/demoapp/src/shared/inventory.service.ts
+++ Angular-practice/demoapp/src/shared/inventory.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Product } from './product.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryService {
+  private orderedProducts:Product[]=[];
+
 
   constructor() { }
+
+  addOrder(product:Product)
+  {
+    this.orderedProducts.push(product);
+    console.log(this.orderedProducts);
+  }
+
+  getOrderedProducts()
+  {
+    return this.orderedProducts;
+  }
+
 }

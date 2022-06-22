4ff2df267827dee01546e247835ce45dbe7f0ba8
cathytsui <48180241+cathy-thy@users.noreply.github.com>
2022-06-23 00:39:08 +0800

    add reactive form

--- Angular-practice/demoapp/src/app/servicecommunication/login/login.component.ts
+++ Angular-practice/demoapp/src/app/servicecommunication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/shared/auth.service';
 
@@ -8,8 +9,19 @@ import { AuthService } from 'src/shared/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username:string;
-  password:string;
+  // username:string;
+  // password:string;
+
+  //Reactive form: have form with class
+  loginForm=new FormGroup({
+    //two instances 
+    username: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required])
+  });
+  
+  get LoginFormControls():any{
+    return this.loginForm['controls'];
+  }
 
   //dependency injection
   //activedRoute only required when their is child route
@@ -21,7 +33,8 @@ export class LoginComponent implements OnInit {
 
   onLogin()
   {
-    let result = this.authservice.onLogin(this.username, this.password);
+    //let result = this.authservice.onLogin(this.username, this.password);
+    let result=this.authservice.onLogin(this.loginForm.value.username,this.loginForm.value.password)
     if(result){
       this.router.navigate(['products'])
     }
